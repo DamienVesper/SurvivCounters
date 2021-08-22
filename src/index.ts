87f747351a7d279602d3eec035e7fec285dc7167
Damien Vesper <ldamienvesper@gmail.com>
2021-08-22 16:51:37 -0500

    add extra proxies

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,13 @@
 // @match        *://thecircleisclosing.com/*
 // @match        *://parmainitiative.com/*
 // @match        *://ot38.club/*
+// @match        *://drchandlertallow.com/*
+// @match        *://rarepotato.com
+// @match        *://archimedesofsyracuse.info/*
+// @match        *://nevelskoygroup.com/*
+// @match        *://kugahi.com/*
+// @match        *://kugaheavyindustry.com/*
+// @match        *://chandlertallowmd.com/*
 // @match        *://c79geyxwmp1zpas3qxbddzrtytffta.ext-twitch.tv/c79geyxwmp1zpas3qxbddzrtytffta/1.0.2/ce940530af57d2615ac39c266fe9679d/*
 // @match        *://*.c79geyxwmp1zpas3qxbddzrtytffta.ext-twitch.tv/c79geyxwmp1zpas3qxbddzrtytffta/1.0.2/ce940530af57d2615ac39c266fe9679d/*
 // @match        *://twitch.tv/popout/survivio/extensions/c79geyxwmp1zpas3qxbddzrtytffta/*
