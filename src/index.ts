f5ad537fcf69d55438bf260df3a0287ff4354c9c
Damien Vesper <ldamienvesper@gmail.com>
2021-08-22 09:58:43 -0500

    chore: lint

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@ import '../assets/scss/common.scss';
 
 import Core from './Core';
 
-const init = (): void => {};
+const init = (): void => {
+    // Start the FPS counter.
+    Core.fpsCounter.calculateFPS();
+};
 
 window.onload = () => {
     init();
