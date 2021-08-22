6c8effe7be0d5ade0588ae4fc58eed4ba270ae4f
Damien Vesper <ldamienvesper@gmail.com>
2021-08-22 15:46:17 -0500

    adren: init

--- src/components/AdrenCounter.ts
+++ src/components/AdrenCounter.ts
@@ -1,5 +1,46 @@
 import Component from '../Component';
 
-class AdrenCounter extends Component {}
+class AdrenCounter extends Component {
+    text: HTMLSpanElement
+
+    constructor () {
+        super();
+
+        this.text = document.createElement(`span`);
+
+        this.element.id = `adrenWrapper`;
+        this.element.classList.add(`box-container`);
+
+        this.element.appendChild(this.text);
+        document.querySelector(`#bottomWrapper`).appendChild(this.element);
+    }
+
+    /**
+     * Calculate current player adrenaline.
+     * @returns number
+     */
+    calculateAdren = (): number => {
+        let adrenPercentage = 0;
+        const boosts = document.querySelectorAll(`.ui-boost-base .ui-bar-inner`);
+
+        boosts.forEach((boost: HTMLDivElement, i: number) => {
+            if (i <= 1) adrenPercentage += parseInt(boost.style.width.slice(0, boost.style.width.length - 1)) / 4;
+            else if (i === 2) adrenPercentage += parseInt(boost.style.width.slice(0, boost.style.width.length - 1)) / 2.5;
+            else if (i === 3) adrenPercentage += parseInt(boost.style.width.slice(0, boost.style.width.length - 1)) / 10;
+        });
+
+        return Math.round(adrenPercentage);
+    }
+
+    /**
+     * Update the adrenaline counter.
+     */
+    updateAdren = (): void => {
+        const adren = this.calculateAdren();
+
+        this.text.innerHTML = `AD: ${adren}`;
+        this.element.hidden = adren === 0;
+    }
+}
 
 export default AdrenCounter;
--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ const init = (): void => {
     // setInterval(Core.counters.ping.updatePing, 1e3); // Update ping every second.
 
     setInterval(Core.counters.health.updateHealth, 25); // Update health every 25ms.
-    // setInterval(Core.counters.adren.updateAdren, 25); // Update adren every 25ms.
+    setInterval(Core.counters.adren.updateAdren, 25); // Update adren every 25ms.
 };
 
 window.onload = () => {
