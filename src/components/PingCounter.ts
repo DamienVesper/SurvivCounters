e3209f4e8d6ad6ec63f19a7296f2063fd1bf5005
Damien Vesper <ldamienvesper@gmail.com>
2022-02-23 18:19:20 -0600

    Revert "failed ping counter"
    This reverts commit ad22f377b435215404b3a00a96c8d06bb9122eac.

--- src/components/PingCounter.ts
+++ src/components/PingCounter.ts
@@ -2,7 +2,6 @@ import Component from '../Component';
 
 class PingCounter extends Component {
     text: HTMLSpanElement;
-    server: string;
     ping: number;
 
     constructor () {
@@ -18,44 +17,15 @@ class PingCounter extends Component {
 
         this.ping = 0;
 
-        // Determine the server that the player is playing on.
-        this.determineServer();
-
-        // Start calculating the ping.
+        // Start calculating the FPS.
         this.calculatePing();
     }
-
-    determineServer = (): void => {
-        const servers = [`na-nyc`, `na-chi`, `na-sfo`, `sa-sao`, `eu-waw`, `eu-fra`, `as-sgp`, `kr-sel`];
-        const pingTime = new Date().valueOf();
-
-        const serverPings: Record<string, number> = {};
-        for (const server of servers) {
-            const request = new XMLHttpRequest();
-
-            request.open(`GET`, `https://${server}-p1.surviv.io`, true);
-            request.onload = () => { serverPings[server] = new Date().valueOf() - pingTime; };
-        }
-
-        let lowestPing: [string, number] = [``, 0];
-        for (const [server, ping] of Object.entries(serverPings)) {
-            if (lowestPing[1] === 0 || ping < lowestPing[1]) lowestPing = [server, ping];
-        }
-
-        this.server = `https://${lowestPing[0]}-p1.surviv.io`;
-    };
 
     /**
      * Calculate the amount of ping the player has.
      */
-    calculatePing = (): void => {
-        const pingTime = new Date().valueOf();
-        const request = new XMLHttpRequest();
-
-        request.open(`GET`, this.server, true);
-        request.onload = () => { this.ping = new Date().valueOf() - pingTime; };
-
-        setTimeout(() => this.calculatePing(), 1e3);
+    calculatePing = (): number => {
+        return 0;
     };
 
     /**
--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ import Core from './Core';
 
 const init = (): void => {
     setInterval(Core.counters.fps.updateFPS, 1e3); // Update FPS every second.
-    setInterval(Core.counters.ping.updatePing, 1e3); // Update ping every second.
+    // setInterval(Core.counters.ping.updatePing, 1e3); // Update ping every second.
 
     setInterval(Core.counters.health.updateHealth, 25); // Update health every 25ms.
     setInterval(Core.counters.adren.updateAdren, 25); // Update adren every 25ms.
